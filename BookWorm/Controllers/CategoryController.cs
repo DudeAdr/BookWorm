ccb8e758732d98650ce52daff8a9f8e6785ebe60
DudeAdr <adrian.dudek02@wp.pl>
2024-06-01 20:57:47 +0200

    Adding Category and css styles

---
+++ BookWorm/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BookWorm.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWorm.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext dbContext;
+        public CategoryController(ApplicationDbContext DbContext) 
+        {
+            dbContext = DbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var CategoryList = dbContext.Categories.ToList();
+            return View(CategoryList);
+        }
+    }
+}
--- BookWorm/Data/ApplicationDbContext.cs
+++ BookWorm/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookWorm.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookWorm.Data
 {
@@ -7,6 +8,16 @@ namespace BookWorm.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fantasy", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Novel", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Action", DisplayOrder = 3 }
+                );
         }
     }
 }
