25968f57cf481035d95360dd289aafa90c614fb9
DudeAdr <adrian.dudek02@wp.pl>
2024-06-02 11:26:03 +0200

    Adding Category form and standard validation

--- BookWorm/Controllers/CategoryController.cs
+++ BookWorm/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using BookWorm.Data;
+using BookWorm.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BookWorm.Controllers
 {
@@ -16,6 +18,31 @@ namespace BookWorm.Controllers
         {
             var CategoryList = dbContext.Categories.ToList();
             return View(CategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category categoryObj)
+        {
+            if(categoryObj.Name == categoryObj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name", "The Display Order can't exactly match the Name");
+            }
+            if(categoryObj.DisplayOrder.ToString() == "0")
+            {
+                ModelState.AddModelError("displayorder", "The Display Order can't be empty");
+            }
+
+            if(ModelState.IsValid)
+            {
+                dbContext.Categories.Add(categoryObj);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
