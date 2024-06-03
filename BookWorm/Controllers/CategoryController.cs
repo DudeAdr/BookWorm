bc8e73b37624306343beefacce0245bf8a9f378a
DudeAdr <adrian.dudek02@wp.pl>
2024-06-03 11:16:54 +0200

    Adding Edit, Delete logic and toastr notifications

--- BookWorm/Controllers/CategoryController.cs
+++ BookWorm/Controllers/CategoryController.cs
@@ -9,7 +9,7 @@ namespace BookWorm.Controllers
     public class CategoryController : Controller
     {
         private ApplicationDbContext dbContext;
-        public CategoryController(ApplicationDbContext DbContext) 
+        public CategoryController(ApplicationDbContext DbContext)
         {
             dbContext = DbContext;
         }
@@ -24,25 +24,83 @@ namespace BookWorm.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Create(Category categoryObj)
         {
-            if(categoryObj.Name == categoryObj.DisplayOrder.ToString()) 
+            if (categoryObj.Name == categoryObj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("name", "The Display Order can't exactly match the Name");
             }
-            if(categoryObj.DisplayOrder.ToString() == "0")
+            if (categoryObj.DisplayOrder.ToString() == "0")
             {
                 ModelState.AddModelError("displayorder", "The Display Order can't be empty");
             }
 
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 dbContext.Categories.Add(categoryObj);
                 dbContext.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category categoryObj)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Categories.Update(categoryObj);
+                dbContext.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = dbContext.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteRecord(int? id)
+        {
+            Category? obj = dbContext.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            dbContext.Categories.Remove(obj);
+            dbContext.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
